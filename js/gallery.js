23b61df369285f8a630eb999b0b9cfe739ceb653
marcyvq12 <marcyvq12@gmail.com>
2020-08-08 09:55:38 -0400

    loggy

--- js/gallery.js
+++ js/gallery.js
@@ -84,18 +84,21 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  // var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  // for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  // }
-  slides[slideIndex-1].style.display = "block";
-  // dots[slideIndex-1].className += " active";
+  try {
+    slides[slideIndex-1].style.display = "block";
+  }
+  catch (err) {
+    conslole.log(slideIndex);
+    console.log(slides[slideIndex-1]);
+  }
   var current_image = slides[slideIndex-1].getElementsByTagName("img")[0];
+  console.log(current_image.clientWidth)
+  console.log(current_image.clientHeight)
   captionText.innerHTML = current_image.title;
 }

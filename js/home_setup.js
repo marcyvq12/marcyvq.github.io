a4c7a96499caacf4f8936e5e4db5dcccad57f774
marcyvq12 <marcyvq12@gmail.com>
2020-09-01 13:14:02 -0400

    time to stop being dumb

--- js/home_setup.js
+++ js/home_setup.js
@@ -1,11 +1,11 @@
 function home_setup() {
-	console.log("we up in here");
 	var modeling = document.getElementById("modeling");
 	var ropeart = document.getElementById("ropeart");
-	var mod_img = modeling.getElementsByTagName("img");
-	var rope_img = ropeart.getElementsByTagName("img");
+	var mod_img = modeling.getElementsByTagName("img")[0];
+	var rope_img = ropeart.getElementsByTagName("img")[0];
 	mod_height = mod_img.clientHeight;
 	rope_height = rope_img.clientHeight;
 	modeling.style.height = mod_height;
 	ropeart.style.height = rope_height;
+	console.log(mod_height);
 }

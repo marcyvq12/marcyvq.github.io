bdc65fc55b8a7999dfcc9f3e0f69748cf1696a05
marcyvq12 <marcyvq12@gmail.com>
2020-09-03 15:59:58 -0400

    doing it for real real shit

--- js/home_setup.js
+++ js/home_setup.js
@@ -1,11 +1,13 @@
+function setCategoryHeights() {
+	var pages = document.getElementsByClassName("page-ref");
+	for (page in pages) {
+		var img = page.getElementsByTagName("img")[0];
+		var img_height = img.clientHeight;
+		page.style.height = img_height.toString().concat("px");
+	}
+}
+
 function home_setup() {
-	var modeling = document.getElementById("modeling");
-	var ropeart = document.getElementById("ropeart");
-	var mod_img = modeling.getElementsByTagName("img")[0];
-	var rope_img = ropeart.getElementsByTagName("img")[0];
-	mod_height = mod_img.clientHeight;
-	rope_height = rope_img.clientHeight;
-	modeling.style.height = mod_height.toString().concat("px");
-	ropeart.style.height = rope_height.toString().concat("px");
-	console.log(modeling.style.height);	
+	// loadCSV(path).then(function(data) {console.log(data)});
+	setCategoryHeights();
 }

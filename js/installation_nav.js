5ba24a59b2f5399a0c992f933e7990b15edfa17a
marcyvq12 <marcyvq12@gmail.com>
2020-09-09 21:49:20 -0400

    first pass installation page

---
+++ js/installation_nav.js
@@ -0,0 +1,32 @@
+function loadPages(data) {
+  var numvids = data.length;
+  var container = document.getElementById("pages-container");
+
+  var imgpath_head = './images/installation/'
+  for (i=0; i < numvids; i++) {
+    var page = document.createElement("div");
+    page.setAttribute("class", "page");
+
+    var link = document.createElement("a");
+    link.href = data[i]['page_name'];
+
+    var img = document.createElement("img");
+    img.src = imgpath_head + data[i]['img_path'];
+
+    var title = document.createElement("div");
+    title.setAttribute("class", "page-title");
+    title.innerHTML = data[i]['title'];
+
+    link.appendChild(img);
+    link.appendChild(title);
+
+    page.appendChild(link);
+    container.appendChild(page);
+  }
+}
+
+function setupInstallations(path) {
+    loadCSV(path).then(function(data) {
+    loadPages(data);
+  });
+}

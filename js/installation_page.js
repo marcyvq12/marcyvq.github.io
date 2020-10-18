bb5d681632851ead46e7b5ebd3976da9fcf3e8d4
marcyvq12 <marcyvq12@gmail.com>
2020-10-17 23:36:19 -0400

    in the weeds

--- js/installation_page.js
+++ js/installation_page.js
@@ -78,7 +78,7 @@ function importProject(path) {
     loadCSV(path).then(function(data) {
     var container = document.getElementById("project-container");
     var numrows = Math.max(data.map(mapper));
-    console.log(data.map(mapper));
+    console.log(numrows);
     for (i=0; i < numrows; i++) {
         row = data.filter(element => element['row'] == i);
         console.log(row);
